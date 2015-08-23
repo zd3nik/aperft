9b06c4cf3aacbe90a80523081154879a94339ce0
Shawn Chidester <zd3nik@gmail.com>
2015-08-23 12:31:30 -0600

    Add simple (linux) testing script
    Example output, run on i7-4900MQ 2.80GHz
    
    $ ./test.sh
    
    80027   aperft.O1
     min KLeafs/sec = 25744.2
     max KLeafs/sec = 79609.8
    
    real    4m3.544s
    user    4m3.829s
    sys     0m0.075s
    
    79455   aperft.O2
     min KLeafs/sec = 32470.1
     max KLeafs/sec = 84952.1
    
    real    3m30.161s
    user    3m30.412s
    sys     0m0.055s
    
    84091   aperft.O3
     min KLeafs/sec = 33097.8
     max KLeafs/sec = 88889.7
    
    real    3m36.842s
    user    3m37.114s
    sys     0m0.071s

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+for i in {1..3}
+do
+  echo
+  g++ -DNDEBUG -O$i aperft.cpp -o aperft.O$i
+  du -b "aperft.O$i"
+  ./aperft > /dev/null || :
+  time ./aperft.O$i epd perftsuite.epd $1 | tail -2
+done
+echo
+
