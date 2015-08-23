9ff9048ae05f2a1c85cdbd1851a78b00d523ffa3
Shawn Chidester <zd3nik@gmail.com>
2015-08-23 12:58:21 -0600

    Fix line that pre-caches aperft binary.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ do
   echo
   g++ -DNDEBUG -O$i aperft.cpp -o aperft.O$i
   du -b "aperft.O$i"
-  ./aperft > /dev/null || :
+  ./aperft.O$i > /dev/null || :
   time ./aperft.O$i epd perftsuite.epd $1 | tail -2
 done
 echo
